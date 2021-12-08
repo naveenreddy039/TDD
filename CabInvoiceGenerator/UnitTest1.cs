4b2e727c3b84381639a71566973e043a5f2c0993
naveen <naveenreddy.k07@gmail.com>
2021-12-08 20:51:08 +0530

    tdd execution

---
+++ CabInvoiceGenerator/UnitTest1.cs
@@ -0,0 +1,38 @@
+using CabInvoiceGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        InvoiceGeneartor invoiceGeneartor = null;
+
+        //public object Assert { get; private set; }
+
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldTotalFare()
+        {
+            invoiceGeneartor = new InvoiceGeneartor(RideType.Normal);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGeneartor.CalcaulateFare(distance, time);
+            double expected = 25;
+
+            Assert.AreEqual(expected, fare);
+
+        }
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            invoiceGeneartor = new InvoiceGeneartor(RideType.Normal);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = invoiceGeneartor.CalcaulateFare(rides);
+            InvoiceSummary excpectedSummary = new InvoiceSummary(2, 30.0);
+
+            Assert.AreEqual(excpectedSummary, summary);
+        }
+
+
+    }
+}
